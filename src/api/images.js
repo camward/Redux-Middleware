fd9c1e5b8208a542e3900dcf0b1e23ccd9292ff7
alex <camward@yandex.ru>
2020-03-08 18:47:01 +0400

    заготовка для api

---
+++ src/api/images.js
@@ -0,0 +1,23 @@
+const URL = `https://api.unsplash.com/photos/`;
+const KEY = '5f96323678d05ff0c4eb264ef184556868e303b32a2db88ecbf15746e6f25e02';
+const PAGE_SIZE = 10;
+
+const fetchImages = async page => {
+  const response = await fetch(`${URL}?client_id=${KEY}&per_page=${PAGE_SIZE}&page=${page}`);
+  const data = await response.json();
+  if (response.status >= 400) {
+    throw new Error(data.errors);
+  }
+  return data;
+};
+
+const fetchImageStats = async id => {
+  const response = await fetch(`${URL}/${id}/statistics?client_id=${KEY}`);
+  const data = await response.json();
+  if (response.status >= 400) {
+    throw new Error(data.errors);
+  }
+  return data;
+};
+
+export { fetchImages, fetchImageStats };
